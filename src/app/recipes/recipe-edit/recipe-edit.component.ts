27f8eb225fab387de0ea314a17d39d26e0d6bc04
surajrocks1997 <surajrocks1997@gmail.com>
2020-08-20 11:30:41 +0530

    deleting ingredient from recipe and some final touches

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -81,6 +81,10 @@ export class RecipeEditComponent implements OnInit {
     );
   }
 
+  onDeleteIngredient(index: number): void {
+    (this.recipeForm.get('ingredients') as FormArray).removeAt(index);
+  }
+
   onCancel(): void {
     this.router.navigate(['../'], { relativeTo: this.route });
   }
