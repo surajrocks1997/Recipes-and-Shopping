a53e06508f5e235400721790a51f080d196b3981
surajrocks1997 <surajrocks1997@gmail.com>
2020-08-21 16:42:25 +0530

    storing recipes in firebase using save data button

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 
+import { DataStorageService } from '../shared/data-storage.service';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
   collapsed = true;
+
+  constructor(private dataStorageService: DataStorageService) {}
+
+  onSaveData(): void {
+    this.dataStorageService.storeRecipes();
+  }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-@Injectable({ providedIn: 'root' })
+import { RecipeService } from '../recipes/recipe.service';
 
+@Injectable({ providedIn: 'root' })
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private recipesService: RecipeService
+  ) {}
+
+  storeRecipes(): void {
+    const recipes = this.recipesService.getRecipes();
+    this.http.put(
+      'https://recipeshopping-445b8.firebaseio.com/recipes.json',
+      recipes
+    ).subscribe(response => {
+      console.log(response);
+    });
+  }
 }
