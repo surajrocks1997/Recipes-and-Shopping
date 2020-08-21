c8d62c328e27398cbcd79d591d29ec692a50434d
surajrocks1997 <surajrocks1997@gmail.com>
2020-08-21 18:22:37 +0530

    handling auth page form input

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent implements OnInit {
+  isLoginMode = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSwitchMode(): void {
+    this.isLoginMode = !this.isLoginMode;
   }
 
+  onSubmit(form: NgForm): void {
+    console.log(form.value);
+    form.reset();
+  }
 }
